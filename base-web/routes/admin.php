db187ab3416c49c8ef06650c09da7b3ff2e2437d
nguyenthiminh04 <nguyenthiminhcute07@gmail.com>
2024-06-04 10:41:48 +0700

    themadmin

--- base-web/routes/admin.php
+++ base-web/routes/admin.php
@@ -6,7 +6,7 @@ $router->mount('/admin', function () use ($router) {
 
     // CRUD USER
     $router->mount('/users', function () use ($router) {
-        $router->get('/',               UserController::class . '@index');  // Danh sách
+        $router->get('/',               UserController::class . '@index');  
         $router->get('/create',         UserController::class . '@create'); // Show form thêm mới
         $router->post('/store',         UserController::class . '@store');  // Lưu mới vào DB
         $router->get('/{id}',           UserController::class . '@show');   // Xem chi tiết
@@ -16,11 +16,3 @@ $router->mount('/admin', function () use ($router) {
     });
     
 });
-
-// $router->get('/admin/users/',               UserController::class . '@index');
-// $router->get('/admin/users/create',         UserController::class . '@create');
-// $router->post('/admin/users/store',         UserController::class . '@store');
-// $router->get('/admin/users/{id}',           UserController::class . '@show');
-// $router->get('/admin/users/{id}/edit',      UserController::class . '@edit');
-// $router->post('/admin/users/{id}/update',   UserController::class . '@update');
-// $router->post('/admin/users/{id}/delete',   UserController::class . '@delete');
---
+++ base-web/src/Controllers/Admin/DashboardController.php
@@ -0,0 +1,4 @@
+<?php
+
+namespace Minhnt\Base\Controllers\Admin;
+
